9f665ca90e83a4328efb66fb7f25b68b0b84ca91
isaemi <imsam0124@gmail.com>
2024-08-26 00:04:32 -0700

    08/26/2024

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
             let height = sec.offsetHeight;
             let id = sec.getAttribute('id');
 
-            if(top >= offset && top < offset + height){
+            if (top >= offset && top < offset + height) {
                 navLinks.forEach(links => {
                     links.classList.remove('active');
-                    document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                     console.log("pressed");
                 })
             }
@@ -30,18 +30,63 @@ document.addEventListener('DOMContentLoaded', () => {
 function showLoader(url) {
     document.querySelector('.loader').style.display = 'block';
 
-    setTimeout(function() {
+    setTimeout(function () {
         window.location.href = url;
     }, 1000);
 }
 
 // Add event listener to the Resume button
-document.querySelector('.btn-group .btn[href="resume.html"]').addEventListener('click', function(event) {
+document.querySelector('.btn-group .btn[href="resume.html"]').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link action
     showLoader('resume.html');
 });
 
-document.addEventListener('DOMContentLoaded',() =>{
-    const background = document.getElementById('background-container');
+document.addEventListener('DOMContentLoaded', () => {
+    const group = document.querySelector('.group');
+    for (let i = 1; i < 50; i++) {
+        const containerId = `iconContainer${i}`;
+        const container = document.createElement('div');
+        container.className = 'iconContainer';
+        container.id = containerId;
+        group.appendChild(container);
+        addIcons(containerId);
+    }
 
-})
+    function addIcons(containerId) {
+        const iconContainer = document.getElementById(containerId);
+        const Unicode = [
+            `\uf001`,
+            `\uf002`,
+            `\uf003`,
+            `\uf004`,
+            `\uf005`,
+            `\uf006`,
+            `\uf007`,
+            `\uf008`,
+            `\uf009`,
+            `\uf010`,
+            `\uf011`,
+            `\uf012`,
+            `\uf013`,
+            `\uf014`,
+            `\uf015`,
+            `\uf016`,
+            `\uf017`,
+            `\uf018`,
+            `\uf019`,
+            `\uf020`,
+        ];
+
+        for (let i = 0; i < 100; i++) {
+            const icon = document.createElement('i');
+            icon.className = 'icon fas';
+            icon.innerHTML = randomIcon(Unicode);
+            iconContainer.appendChild(icon);
+        }
+
+        function randomIcon(values) {
+            const randomIndex = Math.floor(Math.random() * values.length);
+            return values[randomIndex];
+        }
+    }
+});
